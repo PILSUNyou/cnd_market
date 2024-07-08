1b61995e3e7a8d4eef89546e18803f928c50cf10
PILSUNyou <dbyo1125@naver.com>
2024-07-08 18:39:55 +0900

    list에서 detail 바로가기

--- src/main/java/com/cod/market/product/controller/ProductController.java
+++ src/main/java/com/cod/market/product/controller/ProductController.java
@@ -26,7 +26,7 @@ public class ProductController {
 
     @GetMapping("/detail/{id}")
     public String detail(@PathVariable("id") Long id, Model model) {
-
+        model.addAttribute("id", id);
         return "product/detail";
     }
 }
