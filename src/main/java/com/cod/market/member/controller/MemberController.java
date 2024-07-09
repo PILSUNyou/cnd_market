302d5129bee2452aace04f627790ebe8aa6c844d
PILSUNyou <dbyo1125@naver.com>
2024-07-09 14:34:45 +0900

    login & join UI/IX

--- src/main/java/com/cod/market/member/controller/MemberController.java
+++ src/main/java/com/cod/market/member/controller/MemberController.java
@@ -4,21 +4,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController {
+
     @GetMapping("/login")
-    @ResponseBody
     public String login() {
-        return "로그인 페이지입니다.";
+        return "member/login";
     }
 
     @GetMapping("/join")
-    @ResponseBody
     public String join() {
-        return "회원가입 페이지입니다.";
+        return "member/join";
     }
 }
