77b74740420fde87f4608373f1a333ec8c87c03a
PILSUNyou <dbyo1125@naver.com>
2024-07-10 17:35:50 +0900

    question list

--- src/main/java/com/cod/market/product/controller/ProductController.java
+++ src/main/java/com/cod/market/product/controller/ProductController.java
@@ -27,7 +27,10 @@ public class ProductController {
     @GetMapping("/detail/{id}")
     public String detail(@PathVariable("id") Long id, Model model) {
         Product product = productService.getProduct(id);
+
         model.addAttribute("product", product);
+        System.out.println(product.toString());
+
         return "product/detail";
     }
 }
