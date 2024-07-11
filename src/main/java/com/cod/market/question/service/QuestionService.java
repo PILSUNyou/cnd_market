fe1c3c515da03b855f5e5bff85c3b2f0f29c6ff1
PILSUNyou <dbyo1125@naver.com>
2024-07-11 15:29:26 +0900

    modify, delete, create authority

--- src/main/java/com/cod/market/question/service/QuestionService.java
+++ src/main/java/com/cod/market/question/service/QuestionService.java
@@ -42,4 +42,8 @@ public class QuestionService {
 
         questionRepository.save(question);
     }
+
+    public void delete(Question question) {
+        questionRepository.delete(question);
+    }
 }
